a7a24662c34eb5b026fe5b47d7875e009bcbe571
maksim.samorukov <maxim.samorukov@gmail.com>
2026-02-22 19:12:52 +0300

    gestures page

--- src/pages/Gestures/index.tsx
+++ src/pages/Gestures/index.tsx
@@ -1,9 +1,33 @@
-import React from "react";
-
+import React, { useState } from "react";
+import s from "./style.module.scss";
+const imagesNumbers = Array.from({ length: 10 }, (_, i) => i + 1);
 export const Gestures: React.FC = () => {
+  const [selectedImg, setSelectedImg] = useState(imagesNumbers[0]);
+  const setSelectedImageNumber = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setSelectedImg(Number(e.currentTarget.textContent));
+  };
   return (
-    <div>
-      <h1>Gestures Page</h1>
+    <div className={s.container}>
+      <div className={s.menu}>
+        {imagesNumbers.map((i) => (
+          <div className={s.menu__item}>
+            <div className={s.menu__label}>
+              <button onClick={setSelectedImageNumber} className={s.btn}>
+                {i}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={s.field}>
+        <img
+          src={
+            new URL(`./exercises/ex${selectedImg}.png`, import.meta.url).href
+          }
+          loading="lazy"
+          alt={`ex${selectedImg + 1}.png`}
+        />
+      </div>
     </div>
   );
 };
