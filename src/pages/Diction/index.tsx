dc624c358584fe02729f6f7285b52897106bdb3c
maksim.samorukov <maxim.samorukov@gmail.com>
2026-08-10 13:16:23 +0300

    fix

--- src/pages/Diction/index.tsx
+++ src/pages/Diction/index.tsx
@@ -58,10 +58,7 @@ export const Diction: React.FC = () => {
       </div>
       <div className={s.field}>
         <Suspense fallback={<div>loading</div>}>
-          <LazyWrapper
-            path={`../exercises/${currentExrcise.name}/index.tsx`}
-            name={currentExrcise.name}
-          />
+          <LazyWrapper name={currentExrcise.name} />
         </Suspense>
       </div>
     </div>
